efc81ce14631a5ec9840c13823743e7a5b571519
Randi Schreiner <mimirandi21@gmail.com>
2020-11-05 09:15:24 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Randi"
+    end
+
+    get '/hometown' do
+        "My hometown is Prior Lake"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Chesterfield King"
+    end
 end
